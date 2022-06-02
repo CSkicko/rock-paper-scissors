aca196368a8dbf7a87eb955c3d5eb3baa868da93
Craig <craig.skicko@gmail.com>
2022-06-02 21:34:10 +1000

    G and I code copied in

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let compRandNumber;
+
 var player = {
     selection: 0, //r, p, s pick
     won: true,
@@ -15,3 +17,14 @@ if(pick=="r"){
 } else{
     player.selection = 2;
 }
+
+compRandNumber = Math.trunc(Math.random() * 3);
+
+if (player.selection=="r" && compRandNumber=="p"){result="lose"}
+elseif( player.selection=="r" && compRandNumber=="s"){result="win"}
+elseif( player.selection=="s" && compRandNumber=="r"){result="lose"}
+elseif( player.selection=="s" && compRandNumber=="p"){result="win"}
+elseif( player.selection=="p" && compRandNumber=="s"){result="lose"}
+elseif( player.selection=="p" && compRandNumber=="r"){result="win"}
+)
+
