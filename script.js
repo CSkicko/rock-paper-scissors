3cf8b19f0a4e12c8ea8c9a82a52ace8d31b79e1b
Craig <craig.skicko@gmail.com>
2022-06-02 21:24:16 +1000

    Player selection completed

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ var player = {
     draws: 0,
     losses: 0
 }
+
+let pick = prompt("Type r, p or s to pick rock, paper or scissors:").toLowerCase();
+
+if(pick=="r"){
+    player.selection = 0;
+} else if(pick=="p"){
+    player.selection = 1;
+} else{
+    player.selection = 2;
+}
